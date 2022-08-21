bf9649d6f0f5ef029d580eac083e1da86fb216ee
Abdallah Mosa <abdallahmosa95@yahoo.com>
2022-08-21 23:49:58 +0300

    not now

---
+++ class-01/demo/my-first-app/src/compontas/Head.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Head extends React.Component {
+    render(){
+        return(<div>
+        <h1>The first React</h1>
+        </div>)
+    }
+}
+export default Head;
